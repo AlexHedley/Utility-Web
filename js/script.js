6c9160e44f9637e5333611c40327fb07c61c720f
Alex Hedley <alex.hedley@me.com>
2018-10-10 18:18:59 +0100

    Added JSON Pretty Print

--- js/script.js
+++ js/script.js
@@ -191,6 +191,49 @@ function S4() {
 
 // JSON Pretty
 
+
+$('#btnJsonPP').click(function() {
+    var text = $('textarea#json').val();
+    console.log(text);
+    
+    //$(text).blur();
+
+    //var obj = {a:1, 'b':'foo', c:[false,'false',null, 'null', {d:{e:1.3e5,f:'1.3e5'}}]};
+    //var str = JSON.stringify(obj, undefined, 4);
+    var str = JSON.stringify(text, undefined, 4);
+
+    output(str);
+    output(syntaxHighlight(str));
+});
+
+// https://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
+// http://jsfiddle.net/KJQ9K/554/
+
+function output(inp) {
+    //document.body.appendChild(document.createElement('pre')).innerHTML = inp;
+    //$('#output').val(inp);
+    document.getElementById('output').innerHTML = inp;
+}
+
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
 // ---
 
 // XML Pretty
