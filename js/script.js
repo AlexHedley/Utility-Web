94cc9605166ba0a9277b93a6e22fe8a5715271a3
Alex Hedley <alex.hedley@me.com>
2018-10-13 09:11:56 +0100

    Added XML Pretty

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ $(function() {
     hideAll();
 
     $("#section0").show();
+
+    //$("#outputXml").resizable();
+    //$('#outputXml').autoResize();
+    //autosize($('#outputXml'));
 });
 
 function hideAll() {
@@ -212,7 +216,7 @@ $('#btnJsonPP').click(function() {
 function output(inp) {
     //document.body.appendChild(document.createElement('pre')).innerHTML = inp;
     //$('#output').val(inp);
-    document.getElementById('output').innerHTML = inp;
+    document.getElementById('outputJson').innerHTML = inp;
 }
 
 function syntaxHighlight(json) {
@@ -237,6 +241,50 @@ function syntaxHighlight(json) {
 // ---
 
 // XML Pretty
+
+$('#btnXmlPP').click(function() {
+    var text = $('textarea#xml').val();
+    console.log(text);
+
+    var prettyXml = prettifyXml(text);
+    document.getElementById('outputXml').value = prettyXml;
+    $("#outputXml").height( $("#outputXml")[0].scrollHeight );
+});
+
+$('#btnCopyXML').click(function() {
+    var text = $('textarea#outputXml');
+    console.log(text);
+    text.select();
+    document.execCommand("copy");
+    //$(text).blur();
+});
+
+// https://stackoverflow.com/a/47317538/2895831
+// https://jsfiddle.net/klesun/sgeryvyu/369/
+
+var prettifyXml = function(sourceXml)
+{
+    var xmlDoc = new DOMParser().parseFromString(sourceXml, 'application/xml');
+    var xsltDoc = new DOMParser().parseFromString([
+      // describes how we want to modify the XML - indent everything
+        '<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform">',
+        '  <xsl:strip-space elements="*"/>',
+        '  <xsl:template match="para[content-style][not(text())]">', // change to just text() to strip space in text nodes
+        '    <xsl:value-of select="normalize-space(.)"/>',
+        '  </xsl:template>',
+        '  <xsl:template match="node()|@*">',
+        '    <xsl:copy><xsl:apply-templates select="node()|@*"/></xsl:copy>',
+        '  </xsl:template>',
+        '  <xsl:output indent="yes"/>',
+        '</xsl:stylesheet>',
+    ].join('\n'), 'application/xml');
+
+    var xsltProcessor = new XSLTProcessor();    
+    xsltProcessor.importStylesheet(xsltDoc);
+    var resultDoc = xsltProcessor.transformToDocument(xmlDoc);
+    var resultXml = new XMLSerializer().serializeToString(resultDoc);
+    return resultXml;
+};
 
 // ---
 
