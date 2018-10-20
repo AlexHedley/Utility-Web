8434c70ee9b5e5adc482d6b3970d5cbc0e76f208
Alex Hedley <alex.hedley@me.com>
2018-10-20 17:01:38 +0100

    Working on size converter

--- js/script.js
+++ js/script.js
@@ -49,6 +49,15 @@ function hideAll() {
     $("#section11").hide();
     $("#section12").hide();
     $("#section13").hide();
+
+    // $('nav li a').forEach(element => {
+    //     console.log('element:' + element);
+    //     var el = $(this).parent().index();
+    //     var id = "#section" + el;
+    //     $(id).hide();
+    // });
+    // var lis = document.getElementsByTagName("nav").getElementsByTagName("li");
+    // console.log(lis);
 };
 
 $('nav li a').click(function(e) {
@@ -392,3 +401,37 @@ $('#btnCopyBinary').click(function() {
 
 // kb - mb - gb Converter
 
+$('#btnConverter').click(function() {
+    var text = $('textarea#converter').val();
+    console.log(text);
+
+    var converterStr = humanFileSize(text, true);
+    
+    document.getElementById('outputConverter').value = converterStr;
+    $("#outputConverter").height( $("#outputConverter")[0].scrollHeight );
+});
+
+$('#btnCopyConverter').click(function() {
+    var text = $('textarea#outputConverter');
+    console.log(text);
+    text.select();
+    document.execCommand("copy");
+    //$(text).blur();
+});
+
+// https://stackoverflow.com/a/14919494/2895831
+function humanFileSize(bytes, si) {
+    var thresh = si ? 1000 : 1024;
+    if(Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+    var units = si
+        ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+        : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+    var u = -1;
+    do {
+        bytes /= thresh;
+        ++u;
+    } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+    return bytes.toFixed(1)+' '+units[u];
+}
